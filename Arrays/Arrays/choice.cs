9012451b63ea2c64b47018e1365c317c87581a6d
Ken <mangubatken.uclm.edu@gmail.com>
2026-03-09 22:11:08 +0800

    Add files via upload

---
+++ Arrays/Arrays/choice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    internal class choice
+    {
+        public static void menu() {
+
+            Console.Write("Select activity " +
+                "\n1. " +
+                "\n2. " +
+                "\n3." +
+                "\n10. Exit" +
+                "\nSelect Option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option) { 
+                
+                case 1:
+                    Console.WriteLine("You have selected option 1");
+                    break;
+                case 2:
+                    break;
+
+                case >=10:
+                         Console.WriteLine("Exiting...");
+                    break;
+            }
+        }
+    }
+}
---
+++ Arrays/Arrays/main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Arrays;
+
+public class MainClass 
+{
+    public static void Main(String[] args)
+    {
+
+        choice.menu();
+    }
+}
